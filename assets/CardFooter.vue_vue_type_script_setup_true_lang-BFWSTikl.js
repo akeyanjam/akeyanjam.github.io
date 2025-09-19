4daa372616ce07ee688d00455a2988e686d2adf5
Alireza Keyanjam <akj@outlook.com>
2025-09-19 09:28:55 -0400

    login

--- assets/CardFooter.vue_vue_type_script_setup_true_lang-BFWSTikl.js
+++
@@ -1,1 +0,0 @@
-import{d as o,h as r,o as t,n,u as c,D as l,y as p}from"./index-6xlqELlD.js";const f=o({__name:"CardFooter",props:{class:{}},setup(e){const s=e;return(a,d)=>(t(),r("div",{"data-slot":"card-footer",class:n(c(l)("flex items-center px-6 [.border-t]:pt-6",s.class))},[p(a.$slots,"default")],2))}});export{f as _};
